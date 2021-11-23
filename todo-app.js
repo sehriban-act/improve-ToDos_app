4c81399a2fa530eedc6c156f5c98b9f2e5c20e56
sehriban <sehribanmatur9@gmail.com>
2021-11-23 13:27:24 -0500

    form submitted

--- todo-app.js
+++ todo-app.js
@@ -39,21 +39,30 @@ const renderTodos = function (todos, filters) {
         p.textContent = todo.text
         document.querySelector('#todos').appendChild(p)
     })
+
+    const addList = function () {
+        const p = document.createElement('p')
+        p.textContent = document.querySelector('#addTodo').value
+        document.querySelector('#todos').appendChild(addList)
+
+    }
 }
 
 renderTodos(todos, filters)
 
-// Listen for new todo creation
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('Add a new todo...')
-})
-
-// Listen for todo text change
-document.querySelector('#new-todo-text').addEventListener('input', function (e) {
-    console.log(e.target.value)
-})
-
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
+})
+document.querySelector('#addForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.addText.value,
+        completed: false
+
+    })
+    renderTodos(todos, filters)
+    e.target.elements.addText.value = "";
+
+
 })
